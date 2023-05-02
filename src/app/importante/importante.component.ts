cb21ed3cd85c3156e5feafe0817fbdaafabe51ad
Cláudio Fernandes <93228350+claudiorfj@users.noreply.github.com>
2023-05-02 09:27:39 +0100

    create importante component

---
+++ src/app/importante/importante.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-importante',
+  templateUrl: './importante.component.html',
+  styleUrls: ['./importante.component.scss'],
+})
+export class ImportanteComponent  implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {}
+
+}
