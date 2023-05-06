2a650ea8afa297cfebf84dd55afb7869e20771d5
Cláudio Fernandes <93228350+claudiorfj@users.noreply.github.com>
2023-05-06 14:49:53 +0100

    created create-item-modal

---
+++ src/app/components/create-item-modal/create-item-modal.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-create-item-modal',
+  templateUrl: './create-item-modal.component.html',
+  styleUrls: ['./create-item-modal.component.scss'],
+})
+export class CreateItemModalComponent  implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {}
+
+}
