d41214a9f69261ff567d63399c7ebc531cb5fcb6
Cláudio Fernandes <93228350+claudiorfj@users.noreply.github.com>
2023-05-06 11:33:05 +0100

    added content compras.page

--- src/app/pages/compras/compras.page.ts
+++ src/app/pages/compras/compras.page.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { BehaviorSubject } from 'rxjs';
 import { SectorMenu, Labels } from 'src/app/common/data';
+import { IProduct } from 'src/app/interfaces/IProduct';
+import { CartService } from 'src/app/services/cart.service';
 
 @Component({
   selector: 'app-compras',
@@ -11,9 +15,42 @@ export class ComprasPage implements OnInit {
   public sectorMenu = SectorMenu
   public labels = Labels
 
-  constructor() { }
+  cart: IProduct[] = [];
+  products: IProduct[] = [];
+  cartItemCount!: BehaviorSubject<number>;
+
+  constructor(
+    private cartService: CartService,
+    private router: Router,
+  ) {}
 
   ngOnInit() {
+    this.products = this.cartService.getProducts();
+    this.cart = this.cartService.getCart();
+    this.cartItemCount = this.cartService.getCartItemCount();
+  }
+
+  addToCart(product: IProduct) {
+    this.cartService.addProduct(product);
+  }
+
+  getTotal() {
+    const total = this.cart.reduce((accumulator, product) => {
+      return accumulator + product.price * product.amount;
+    }, 0);
+    return total;
+  }
+
+  decreaseCartItem(product: IProduct) {
+    this.cartService.decreaseProduct(product);
+  }
+
+  increaseCartItem(product: IProduct) {
+    this.cartService.addProduct(product);
+  }
+
+  removeCartItem(product: IProduct) {
+    this.cartService.removeProduct(product);
   }
 
 }
